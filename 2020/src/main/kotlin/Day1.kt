fa7f01c22011505e145458b9d1885cad6bb81b80
Kevin Hobson <kevinroyhobson@gmail.com>
2020-12-01 16:17:58 -0500

    2020 Day 1

---
+++ 2020/src/main/kotlin/Day1.kt
@@ -0,0 +1,47 @@
+import java.io.File
+
+class Day1 {
+
+    private val _inputFilePath = "input/2020-12-01_input-1.txt"
+    private val _theYear = 2020
+
+    public fun puzzle1() : Int {
+
+        val inputIntsSet = getInputIntegers().toHashSet()
+
+        for (number in inputIntsSet) {
+
+            val thisNumberPair = _theYear - number
+            if (inputIntsSet.contains(thisNumberPair)) {
+                return number * thisNumberPair
+            }
+        }
+
+        throw Exception("No two numbers add up to 2020.")
+    }
+
+
+    public fun puzzle2() : Int {
+
+        val inputIntsSet = getInputIntegers().toHashSet()
+
+        for (a in inputIntsSet) {
+            for (b in inputIntsSet) {
+
+                val c = _theYear - a - b
+                if (inputIntsSet.contains(c)) {
+                    return a * b * c
+                }
+
+            }
+        }
+
+        throw Exception("No three numbers add up to 2020.")
+    }
+
+    private fun getInputIntegers() : List<Int> {
+        return File(_inputFilePath).readLines()
+                                   .map { it.toInt() }
+    }
+
+}
---
+++ 2020/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+
+    val day1 = Day1()
+    println(day1.puzzle1())
+    println(day1.puzzle2())
+
+}
