07318015f5bffc3ca347430f6dbdb5610fe3ede1
kevinroyhobson <kevinroyhobson@gmail.com>
2020-12-06 10:11:19 -0500

    day 6 puzzle 2

--- 2020/src/main/kotlin/Day6.kt
+++ 2020/src/main/kotlin/Day6.kt
@@ -4,7 +4,7 @@ class Day6 {
 
     private val _inputFilePath = "input/2020-12-06.txt"
 
-    fun puzzle1(): Int {
+    fun thePuzzle(): Int {
 
         val input = File(_inputFilePath).readLines()
         var yesAnswerSum = 0
@@ -15,7 +15,7 @@ class Day6 {
             currentAnswerGroup.processPersonAnswers(personAnswers)
 
             if (personAnswers.isBlank()){
-                yesAnswerSum += currentAnswerGroup.howManyQuestionsAreAnsweredYesByAnyone()
+                yesAnswerSum += currentAnswerGroup.howManyQuestionsAreAnsweredYesByEveryone()
                 currentAnswerGroup = AnswerGroup()
             }
         }
@@ -26,13 +26,26 @@ class Day6 {
 
 class AnswerGroup() {
 
-    private val _questionsAnsweredYesByAnyone = HashSet<Char>()
+    private val _questionsAnsweredYesByAnyone = hashSetOf<Char>()
+    private var _questionsAnsweredYesByEveryone = listOf('a'..'z').flatten()
 
     fun processPersonAnswers(personAnswers: String) {
+
+        if (personAnswers.isBlank()) {
+            return
+        }
+
         personAnswers.forEach { _questionsAnsweredYesByAnyone.add(it) }
+
+        var thisYesSet = personAnswers.toHashSet()
+        _questionsAnsweredYesByEveryone = _questionsAnsweredYesByEveryone.filter { thisYesSet.contains(it) }
     }
 
     fun howManyQuestionsAreAnsweredYesByAnyone() : Int {
         return _questionsAnsweredYesByAnyone.count()
+    }
+
+    fun howManyQuestionsAreAnsweredYesByEveryone() : Int {
+        return _questionsAnsweredYesByEveryone.count()
     }
 }
