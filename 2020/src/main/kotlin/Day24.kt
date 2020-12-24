8831dc40d39d2c5bd0842e181644adeb1e3ddc60
Kevin Hobson <kevinroyhobson@gmail.com>
2020-12-24 12:39:54 -0500

    day 24 puzzle 2

--- 2020/src/main/kotlin/Day24.kt
+++ 2020/src/main/kotlin/Day24.kt
@@ -4,9 +4,46 @@ class Day24 {
 
     private val _inputFilePath = "input/2020-12-24.txt"
 
-    private val _numTimesFlippedByTileCoordinate = hashMapOf<Coordinate, Int>()
-
     fun puzzle1() : Int {
+        return getBlackTilesAfterInputIsFlipped().count()
+    }
+
+    fun puzzle2() : Int {
+
+        var currentBlackTileSet = getBlackTilesAfterInputIsFlipped()
+
+        for (i in 1..100) {
+
+            var nextBlackTileSet = hashSetOf<Coordinate>()
+
+            for (blackTile in currentBlackTileSet) {
+
+                var numAdjacentBlackTiles = getNumBlackTilesAdjacentToCoordinate(blackTile, currentBlackTileSet)
+                if (numAdjacentBlackTiles in 1..2) {
+                    nextBlackTileSet.add(blackTile)
+                }
+            }
+
+            var whiteTilesToProcess = currentBlackTileSet.flatMap { getCoordinatesAdjacentToCoordinate(it) }
+                                                         .filter { !currentBlackTileSet.contains(it) }
+
+            for (whiteTile in whiteTilesToProcess) {
+                var numAdjacentBlackTiles = getNumBlackTilesAdjacentToCoordinate(whiteTile, currentBlackTileSet)
+                if (numAdjacentBlackTiles == 2) {
+                    nextBlackTileSet.add(whiteTile)
+                }
+            }
+
+            println("Day $i: ${nextBlackTileSet.count()}")
+            currentBlackTileSet = nextBlackTileSet
+        }
+
+        return currentBlackTileSet.count()
+    }
+
+    private fun getBlackTilesAfterInputIsFlipped() : HashSet<Coordinate> {
+
+        var numTimesFlippedByTileCoordinate = hashMapOf<Coordinate, Int>()
 
         var tileLines = File(_inputFilePath).readLines()
 
@@ -23,13 +60,14 @@ class Day24 {
 
             }
 
-            var numTimesThisTileFlipped = _numTimesFlippedByTileCoordinate.getOrDefault(targetCoordinate, 0)
+            var numTimesThisTileFlipped = numTimesFlippedByTileCoordinate.getOrDefault(targetCoordinate, 0)
             numTimesThisTileFlipped++
-            _numTimesFlippedByTileCoordinate[targetCoordinate] = numTimesThisTileFlipped
+            numTimesFlippedByTileCoordinate[targetCoordinate] = numTimesThisTileFlipped
         }
 
-        var numTilesFlippedToBlack = _numTimesFlippedByTileCoordinate.values.count { it % 2 == 1 }
-        return numTilesFlippedToBlack
+        return numTimesFlippedByTileCoordinate.filter { it.value % 2 == 1 }
+                                              .keys
+                                              .toHashSet()
     }
 
 
@@ -53,6 +91,16 @@ class Day24 {
             'e', 'w' -> directionString[index].toString()
             else -> directionString.substring(index, index + 2)
         }
+    }
+
+    private fun getCoordinatesAdjacentToCoordinate(coordinate: Coordinate) : List<Coordinate> {
+        return _coordinateShiftByDirection.values
+                                          .map { Coordinate(coordinate.x + it.x, coordinate.y + it.y) }
+    }
+
+    private fun getNumBlackTilesAdjacentToCoordinate(coordinate: Coordinate, blackTileSet: HashSet<Coordinate>) : Int {
+        var adjacentCoordinates = getCoordinatesAdjacentToCoordinate(coordinate)
+        return adjacentCoordinates.count { blackTileSet.contains(it) }
     }
 
     private val _directionByAbbreviation = hashMapOf("e" to Direction.East,
