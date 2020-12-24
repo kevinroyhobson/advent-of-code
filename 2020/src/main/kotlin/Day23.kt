e629103c0dc0a3978ca91b4edb79f1ab34a72494
Kevin Hobson <kevinroyhobson@gmail.com>
2020-12-24 15:28:24 -0500

    updated day 23 puzzle 1 to use a proper list of integers in preparation for puzzle 2

--- 2020/src/main/kotlin/Day23.kt
+++ 2020/src/main/kotlin/Day23.kt
@@ -1,6 +1,6 @@
 class Day23 {
 
-    var _allCupsInOrder = "469217538"
+    var _allCupsInOrder = arrayListOf(4, 6, 9, 2, 1, 7, 5, 3, 8)
     var _currentCupIndex = 0
     var _currentCupValue = 0
     var _destinationCupIndex = 0
@@ -10,25 +10,26 @@ class Day23 {
 
         for (i in 1..100) {
 
-            _currentCupValue = _allCupsInOrder[_currentCupIndex].toString().toInt()
+            _currentCupValue = _allCupsInOrder[_currentCupIndex]
             setPickedUpCupIndexes()
             setDestinationCupIndex()
 
-            var nextCupOrder = _allCupsInOrder[_destinationCupIndex].toString()
-            _pickedUpCupsIndices.forEach { nextCupOrder += _allCupsInOrder[it] }
+            var nextCupOrder = arrayListOf<Int>()
+            nextCupOrder.add(_allCupsInOrder[_destinationCupIndex])
+            _pickedUpCupsIndices.forEach { nextCupOrder.add(_allCupsInOrder[it]) }
 
-            var nextCupIndex = (_destinationCupIndex + 1) % _allCupsInOrder.length
-            while (nextCupOrder.length < _allCupsInOrder.length) {
+            var nextCupIndex = (_destinationCupIndex + 1) % _allCupsInOrder.count()
+            while (nextCupOrder.count() < _allCupsInOrder.count()) {
 
                 if (!_pickedUpCupsIndices.contains(nextCupIndex)) {
-                    nextCupOrder += _allCupsInOrder[nextCupIndex]
+                    nextCupOrder.add(_allCupsInOrder[nextCupIndex])
                 }
 
-                nextCupIndex = (nextCupIndex + 1) % _allCupsInOrder.length
+                nextCupIndex = (nextCupIndex + 1) % _allCupsInOrder.count()
             }
 
-            var newIndexOfCurrentCup = nextCupOrder.indexOf(_currentCupValue.toString())
-            var nextCurrentCupIndex = (newIndexOfCurrentCup + 1) % nextCupOrder.length
+            var newIndexOfCurrentCup = nextCupOrder.indexOf(_currentCupValue)
+            var nextCurrentCupIndex = (newIndexOfCurrentCup + 1) % nextCupOrder.count()
 
             _allCupsInOrder = nextCupOrder
             _currentCupIndex = nextCurrentCupIndex
@@ -42,31 +43,31 @@ class Day23 {
 
         _pickedUpCupsIndices.clear()
 
-        var currentIndexToUse = (_currentCupIndex + 1) % _allCupsInOrder.length
+        var currentIndexToUse = (_currentCupIndex + 1) % _allCupsInOrder.count()
         for (x in 1..3) {
             _pickedUpCupsIndices.add(currentIndexToUse)
 
-            currentIndexToUse = (currentIndexToUse + 1) % _allCupsInOrder.length
+            currentIndexToUse = (currentIndexToUse + 1) % _allCupsInOrder.count()
         }
     }
 
     private fun setDestinationCupIndex() {
 
-        var pickedUpCupValues = _pickedUpCupsIndices.map { _allCupsInOrder[it].toString().toInt() }
+        var pickedUpCupValues = _pickedUpCupsIndices.map { _allCupsInOrder[it] }
                                                     .toHashSet()
 
         var destinationCupValue = _currentCupValue - 1
         if (destinationCupValue == 0) {
-            destinationCupValue = _allCupsInOrder.length
+            destinationCupValue = _allCupsInOrder.count()
         }
 
         while (pickedUpCupValues.contains(destinationCupValue)) {
             destinationCupValue--
             if (destinationCupValue == 0) {
-                destinationCupValue = _allCupsInOrder.length
+                destinationCupValue = _allCupsInOrder.count()
             }
         }
 
-        _destinationCupIndex = _allCupsInOrder.indexOf(destinationCupValue.toString())
+        _destinationCupIndex = _allCupsInOrder.indexOf(destinationCupValue)
     }
 }
