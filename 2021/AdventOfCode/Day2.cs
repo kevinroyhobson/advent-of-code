bab162c90312c7590b39be92ef9c7b2c38ba6622
kevinroyhobson <kevinroyhobson@gmail.com>
2021-12-02 09:00:28 -0500

    day 2 puzzle 2

--- 2021/AdventOfCode/Day2.cs
+++ 2021/AdventOfCode/Day2.cs
@@ -41,5 +41,42 @@ namespace AdventOfCode
 
             return horizontalPosition * depth;
         }
+        
+        public int Puzzle2()
+        {
+            int horizontalPosition = 0;
+            int depth = 0;
+            int aim = 0;
+
+            var commands = File.ReadLines(InputPath);
+
+            foreach (var command in commands)
+            {
+                var commandComponents = command.Split(' ');
+                string commandType = commandComponents[0];
+                int amount = int.Parse(commandComponents[1]);
+
+                switch (commandType)
+                {
+                    case "forward":
+                        horizontalPosition += amount;
+                        depth += (aim * amount);
+                        break;
+                    
+                    case "down":
+                        aim += amount;
+                        break;
+                        
+                    case "up":
+                        aim -= amount;
+                        break;
+                    
+                    default:
+                        throw new ArgumentException("Unknown command type.");
+                }
+            }
+
+            return horizontalPosition * depth;
+        }
     }
 }
