7be3b7548018eef7bc01bb5060ba32af6c18e2ac
kevinroyhobson <kevinroyhobson@gmail.com>
2021-12-05 00:55:25 -0500

    day 5 puzzle 2

--- 2021/AdventOfCode/Day5.cs
+++ 2021/AdventOfCode/Day5.cs
@@ -60,8 +60,73 @@ namespace AdventOfCode
 
             return numDangerousAreas;
         }
+        
+        public int Puzzle2()
+        {
+            var input = File.ReadLines(InputPath)
+                            .ToList();
 
+            var ventLines = input.Select(i => new VentLine(i));
+            var ventLineEdges = new[]
+            {
+                ventLines.Max(vl => vl.xStart),
+                ventLines.Max(vl => vl.xEnd),
+                ventLines.Max(vl => vl.yStart),
+                ventLines.Max(vl => vl.yEnd)
+            };
 
+            var oceanSize = ventLineEdges.Max() + 1;
+
+            int[,] oceanMap = new int[oceanSize, oceanSize];
+            foreach (var ventLine in ventLines)
+            {
+                if (ventLine.xStart == ventLine.xEnd)
+                {
+                    int yMin = Math.Min(ventLine.yStart, ventLine.yEnd);
+                    int yMax = Math.Max(ventLine.yStart, ventLine.yEnd);
+                    for (int y = yMin; y <= yMax; y++)
+                    {
+                        oceanMap[ventLine.xStart, y]++;
+                    }
+                }
+
+                else if (ventLine.yStart == ventLine.yEnd)
+                {
+                    int xMin = Math.Min(ventLine.xStart, ventLine.xEnd);
+                    int xMax = Math.Max(ventLine.xStart, ventLine.xEnd);
+                    for (int x = xMin; x <= xMax; x++)
+                    {
+                        oceanMap[x, ventLine.yStart]++;
+                    }
+                }
+
+                else // Line is perfectly diagonal
+                {
+                    int xStep = ventLine.xEnd - ventLine.xStart > 0 ? 1 : -1;
+                    int yStep = ventLine.yEnd - ventLine.yStart > 0 ? 1 : -1;
+                    int numLocations = Math.Abs(ventLine.xEnd - ventLine.xStart) + 1;
+
+                    for (int i = 0; i < numLocations; i++)
+                    {
+                        int thisX = ventLine.xStart + (i * xStep);
+                        int thisY = ventLine.yStart + (i * yStep);
+                        oceanMap[thisX, thisY]++;
+                    }
+                }
+            }
+
+            int numDangerousAreas = 0;
+            foreach (var location in oceanMap)
+            {
+                if (location > 1)
+                {
+                    numDangerousAreas++;
+                }
+            }
+
+            return numDangerousAreas;
+        }
+        
         private class VentLine
         {
             public VentLine(string inputLine)
@@ -81,11 +146,11 @@ namespace AdventOfCode
                                           [1]);
             }
             
-            public int xStart { get; set; }
-            public int yStart { get; set; }
+            public int xStart { get; }
+            public int yStart { get; }
             
-            public int xEnd { get; set; }
-            public int yEnd { get; set; }
+            public int xEnd { get; }
+            public int yEnd { get; }
         }
     }
 }
