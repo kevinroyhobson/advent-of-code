b714f1ec8bd932fb77dc8abc0e5525cd94e4037f
kevinroyhobson <kevinroyhobson@gmail.com>
2021-12-07 07:38:55 -0500

    day 7 part 2

--- 2021/AdventOfCode/Day7.cs
+++ 2021/AdventOfCode/Day7.cs
@@ -33,5 +33,46 @@ namespace AdventOfCode
 
             return minFuelRequired;
         }
+        
+        public long Puzzle2()
+        {
+            var crabPositions = File.ReadAllText(InputPath)
+                                    .Split(',')
+                                    .Select(int.Parse);
+
+            int minCrabPosition = crabPositions.Min();
+            int maxCrabPosition = crabPositions.Max();
+            
+            int minFuelRequired = Int32.MaxValue;
+
+            for (int candidateAlignmentPosition = minCrabPosition; 
+                candidateAlignmentPosition <= maxCrabPosition;
+                candidateAlignmentPosition++)
+            {
+                int totalFuelRequired = crabPositions.Sum(crab => GetFuelRequiredToMoveDistance(Math.Abs(candidateAlignmentPosition - crab)));
+                if (totalFuelRequired < minFuelRequired)
+                {
+                    minFuelRequired = totalFuelRequired;
+                }
+            }
+
+            return minFuelRequired;
+        }
+
+        private int GetFuelRequiredToMoveDistance(int distance)
+        {
+            if (distance == 0)
+            {
+                return 0;
+            }
+            
+            if (!_fuelRequiredByDistance.ContainsKey(distance))
+            {
+                _fuelRequiredByDistance[distance] = GetFuelRequiredToMoveDistance(distance - 1) + distance;
+            }
+            
+            return _fuelRequiredByDistance[distance];
+        }
+        private readonly Dictionary<int, int> _fuelRequiredByDistance = new();
     }
 }
