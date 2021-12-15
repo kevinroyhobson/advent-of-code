1e4b6d14195dab904fba08c636bc45a1dc5f99ec
kevinroyhobson <kevinroyhobson@gmail.com>
2021-12-15 13:16:51 -0500

    day 15 part 2

--- 2021/AdventOfCode/Day15.cs
+++ 2021/AdventOfCode/Day15.cs
@@ -12,70 +12,150 @@ namespace AdventOfCode
         private int[,] _riskLevelMap;
         private int?[,] _knownMinimumRiskMap;
         
+        private int _xLength;
+        private int _yLength;
+
+        private bool[,] _hasPointBeenProcessed;
+        private HashSet<Tuple<int, int>> _candidateNextPoints;
+        
         public int Puzzle1()
         {
             var input = File.ReadLines(InputPath)
                             .ToList();
 
-            var xLength = input.First().Length;
-            var yLength = input.Count;
+            _xLength = input.First().Length;
+            _yLength = input.Count;
             
-            _riskLevelMap = new int[xLength, yLength];
-            _knownMinimumRiskMap = new int?[xLength, yLength];
-            for (int y = 0; y < yLength; y++)
+            _riskLevelMap = new int[_xLength, _yLength];
+            for (int y = 0; y < _yLength; y++)
             {
-                for (int x = 0; x < xLength; x++)
+                for (int x = 0; x < _xLength; x++)
                 {
                     _riskLevelMap[x, y] = int.Parse(input[y].Substring(x, 1));
                 }
             }
 
-            var pointsToProcess = new Queue<Tuple<int, int>>();
-            _knownMinimumRiskMap[xLength - 1, yLength - 1] = _riskLevelMap[xLength - 1, yLength - 1];
-            pointsToProcess.Enqueue(Tuple.Create(xLength - 2, yLength - 1));
-            pointsToProcess.Enqueue(Tuple.Create(xLength - 1, yLength - 2));
+            ComputeKnownRiskMap();
+            return _knownMinimumRiskMap[_xLength - 1, _yLength - 1].Value;
+        }
 
-            while (pointsToProcess.Any())
+        public int Puzzle2()
+        {
+            var input = File.ReadLines(InputPath)
+                            .ToList();
+
+            int tileXLength = input.First().Length;
+            int tileYLength = input.Count;
+
+            _xLength = tileXLength * 5;
+            _yLength = tileYLength * 5;
+            
+            _riskLevelMap = new int[_xLength, _yLength];
+            for (int y = 0; y < tileXLength; y++)
             {
-                var thisPoint = pointsToProcess.Dequeue();
-                var x = thisPoint.Item1;
-                var y = thisPoint.Item2;
-
-                if (_knownMinimumRiskMap[x, y].HasValue)
+                for (int x = 0; x < tileYLength; x++)
                 {
-                    continue;
-                }
+                    int originalRisk = int.Parse(input[y].Substring(x, 1));
 
-                int minimumRiskFromHere = Int32.MaxValue;
-                if (IsPointOnMap(x + 1, y))
-                {
-                    minimumRiskFromHere = Math.Min(minimumRiskFromHere, _knownMinimumRiskMap[x + 1, y].Value);
-                }
-
-                if (IsPointOnMap(x, y + 1))
-                {
-                    minimumRiskFromHere = Math.Min(minimumRiskFromHere, _knownMinimumRiskMap[x, y + 1].Value);
-                }
-
-                _knownMinimumRiskMap[x, y] = _riskLevelMap[x, y] + minimumRiskFromHere;
-
-                if (IsPointOnMap(x - 1, y))
-                {
-                    pointsToProcess.Enqueue(Tuple.Create(x - 1, y));
-                }
-
-                if (IsPointOnMap(x, y - 1))
-                {
-                    pointsToProcess.Enqueue(Tuple.Create(x, y - 1));
+                    for (int i = 0; i < 5; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            int globalX = tileXLength * i + x;
+                            int globalY = tileYLength * j + y;
+                            int adjustedRisk = GetAdjustedRisk(originalRisk, i, j);
+                            _riskLevelMap[globalX, globalY] = adjustedRisk;
+                        }
+                    }
                 }
             }
 
-            return _knownMinimumRiskMap[0, 0].Value - _riskLevelMap[0, 0];
+            ComputeKnownRiskMap();
+            return _knownMinimumRiskMap[_xLength - 1, _yLength - 1].Value;
+        }
+
+        private int GetAdjustedRisk(int originalRisk, int xOffset, int yOffset)
+        {
+            int adjustedRisk = originalRisk + xOffset + yOffset;
+            if (adjustedRisk > 9)
+            {
+                adjustedRisk -= 9;
+            }
+
+            return adjustedRisk;
+        }
+
+        private void ComputeKnownRiskMap()
+        {
+            _knownMinimumRiskMap = new int?[_xLength, _yLength];
+            _hasPointBeenProcessed = new bool[_xLength, _yLength];
+            
+            _knownMinimumRiskMap[0, 0] = 0;
+            _candidateNextPoints = new HashSet<Tuple<int, int>>() {Tuple.Create(0, 0)};
+
+            int numProcessedPoints = 0;
+            while (!_hasPointBeenProcessed[_xLength - 1, _yLength - 1])
+            {
+                var currentPoint = GetNextPointToProcess();
+                int x = currentPoint.Item1;
+                int y = currentPoint.Item2;
+
+                var neighborsToCheck = new List<Tuple<int, int>>()
+                {
+                    Tuple.Create(x - 1, y),
+                    Tuple.Create(x + 1, y),
+                    Tuple.Create(x, y - 1),
+                    Tuple.Create(x, y + 1),
+                };
+                
+                foreach (var neighborToCheck in neighborsToCheck.Where(point => IsPointOnMap(point.Item1, point.Item2)))
+                {
+                    int neighborX = neighborToCheck.Item1;
+                    int neighborY = neighborToCheck.Item2;
+                    int riskViaThisPath = _knownMinimumRiskMap[x, y].Value + _riskLevelMap[neighborToCheck.Item1, neighborToCheck.Item2];
+
+                    if (!_knownMinimumRiskMap[neighborX, neighborY].HasValue ||
+                        riskViaThisPath < _knownMinimumRiskMap[neighborX, neighborY])
+                    {
+                        _knownMinimumRiskMap[neighborX, neighborY] = riskViaThisPath;
+                        if (!_hasPointBeenProcessed[neighborX, neighborY])
+                        {
+                            _candidateNextPoints.Add(Tuple.Create(neighborX, neighborY));
+                        }
+                    }
+                }
+
+                _hasPointBeenProcessed[x, y] = true;
+                _candidateNextPoints.Remove(Tuple.Create(x, y));
+                numProcessedPoints++;
+                if (numProcessedPoints % 100 == 0)
+                {
+                    Console.WriteLine($"processed {numProcessedPoints} points");
+                }
+            }
+        }
+
+        private Tuple<int, int> GetNextPointToProcess()
+        {
+            int minUnprocessedPointRiskValue = int.MaxValue;
+            Tuple<int, int> minUnprocessedPoint = null;
+
+            foreach (var candidateNextPoint in _candidateNextPoints)
+            {
+                int currentRiskValueAtThisPoint = _knownMinimumRiskMap[candidateNextPoint.Item1, candidateNextPoint.Item2].Value;
+                if (currentRiskValueAtThisPoint < minUnprocessedPointRiskValue)
+                {
+                    minUnprocessedPointRiskValue = currentRiskValueAtThisPoint;
+                    minUnprocessedPoint = candidateNextPoint;
+                }
+            }
+
+            return minUnprocessedPoint;
         }
 
         private bool IsPointOnMap(int x, int y)
         {
-            return x >= 0 && y >= 0 && x < _riskLevelMap.GetLength(0) && y < _riskLevelMap.GetLength(1);
+            return x >= 0 && y >= 0 && x < _xLength && y < _yLength;
         }
     }
 }
