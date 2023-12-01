cd7727a4ce07a3bbf41b2c8fc315e7a4b2dd5ba3
Kevin Hobson <kevinroyhobson@gmail.com>
2023-12-01 00:34:35 -0500

    day 1 puzzle 1

---
+++ 2023/AdventOfCode/Day1.cs
@@ -0,0 +1,40 @@
+﻿public class Day1
+{
+    private const string InputPath = "input/2023-12-01.txt";
+    public int Puzzle1()
+    {
+        return File.ReadAllLines(InputPath)
+            .Select(line => 10 * GetFirstInt(line) + GetLastInt(line))
+            .Sum();
+    }
+    
+    private int GetFirstInt(string line)
+    {
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (char.IsDigit(line[i]))
+            {
+                return Int32.Parse(line[i].ToString());
+            }
+        }
+
+        throw new ApplicationException("No integer found in line");
+    }
+    
+    private int GetLastInt(string line)
+    {
+        for (int i = line.Length - 1; i >= 0; i--)
+        {
+            if (char.IsDigit(line[i]))
+            {
+                return Int32.Parse(line[i].ToString());
+            }
+        }
+
+        throw new ApplicationException("No integer found in line");
+    }
+    
+    
+}
+
+
---
+++ 2023/AdventOfCode/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(new Day1().Puzzle1());
