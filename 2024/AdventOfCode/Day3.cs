ef0e7cbb936fdf7a00266910c7e8d6892bdf84ac
Kevin Hobson <kevinroyhobson@gmail.com>
2024-12-19 17:43:40 -0500

    day 3 puzzle 2

--- 2024/AdventOfCode/Day3.cs
+++ 2024/AdventOfCode/Day3.cs
@@ -14,4 +14,31 @@ public class Day3
         MatchCollection matches = Regex.Matches(input, pattern);
         return matches.Sum(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
     }
+
+    public int Puzzle2()
+    {
+        string input = File.ReadAllText(_inputPath);
+        string pattern = @"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)";
+        
+        int retVal = 0;
+        bool isMultiplyingEnabled = true;
+        MatchCollection matches = Regex.Matches(input, pattern);
+        foreach(Match match in matches)
+        {
+            if(match.Value == "do()")
+            {
+                isMultiplyingEnabled = true;
+            }
+            else if(match.Value == "don't()")
+            {
+                isMultiplyingEnabled = false;
+            }
+            else if(isMultiplyingEnabled)
+            {
+                retVal += (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+            }
+        }
+
+        return retVal;
+    }
 }
--- 2024/AdventOfCode/Program.cs
+++ 2024/AdventOfCode/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode;
 
-Console.WriteLine(new Day3().Puzzle1());
+Console.WriteLine(new Day3().Puzzle2());
